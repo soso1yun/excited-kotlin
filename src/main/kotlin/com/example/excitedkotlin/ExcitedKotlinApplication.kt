7aa13023f79a00567526f1a3030ba1b90a3d6c26
soso1yun-at-522339398443 <zRKAKNnlV/Wv5HpoLWLVokbcebhW2ZErcyhCMzygReU=>
2024-02-20 22:12:55 +0900

    first commit

---
+++ src/main/kotlin/com/example/excitedkotlin/ExcitedKotlinApplication.kt
@@ -0,0 +1,14 @@
+package com.example.excitedkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcitedKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExcitedKotlinApplication>(*args)
+}
+
+
+//excited-kotlin
---
+++ src/test/kotlin/com/example/excitedkotlin/ExcitedKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.excitedkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcitedKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
